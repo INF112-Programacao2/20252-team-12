0ad9320d0f4da456bc1f2d0af4dc3bd88c2864c7
João Victor Moreira Carvalho <joaovmoreira.carvalho@gmail.com>
2025-11-17 02:39:42 -0300

    Implementação devolverLivro() e função caseInsentivite()

--- Estudante.cpp
+++ Estudante.cpp
@@ -1,7 +1,22 @@
 #include "Estudante.hpp"
 #include "Usuario.hpp"
 #include <limits>
+#include <stdexcept>
 #include <iostream>
+
+// Funcao de comparacao case insensitive para conferir nomes de livros
+bool caseInsensitiveComp(const std::string a, const std::string b) {
+    if (a.size() != b.size())
+        return false;
+
+    for (size_t i = 0; i < a.size(); i++) {
+        if (std::toupper((unsigned char)a[i]) != std::toupper((unsigned char)b[i]))
+            return false;
+    }
+
+    return true;
+}
+
 
 Estudante::Estudante(const std::string& _nome, const std::string& _data_de_nascimento, const std::string& _email, const std::string& _senha, const std::string& _matricula, const std::string& _curso) : Usuario(_nome,_data_de_nascimento, _email, _senha), matricula(_matricula), curso(_curso), emprestimos(){
     this->carteirinha = new Carteirinha();
@@ -30,15 +45,74 @@ void Estudante::pegarLivro(const Biblioteca& biblioteca){
     // this->empréstimos.push_back(empréstimo)
 }
 void Estudante::devolverLivro(const Biblioteca& biblioteca){
-    // Lógica para escolher livro e devolver
+    if (this->get_emprestimos().size() == 0) {
+        std::cout << "Nao ha emprestimos pendentes\n";
+        return;
+    }
+    
+    for (auto emprestimo : this->get_emprestimos()) {
+        std::cout << emprestimo->getLivro()->getTitulo() << std::endl;
+    }
+    
+    std::cout << "Escolha qual livro deseja devolver: \n";
+    
+    std::string titulo_livro_devolvido;
+    Emprestimo* livro_devolvido = nullptr;
+    std::getline(std::cin, titulo_livro_devolvido);
 
-    // Empréstimo* empréstimo_do_livro = (Empréstimo)
-    // if (empréstimo_do_livro->calculaMulta() > 0.0) -> perguntar se deseja debitar da carteirinha
-    // else if (empréstimo_do_livro->calculaMulta() == 0.0)
-    // empréstimo_do_livro->setDevolvido(true)
-    // empréstimo_do_livro->livro->setNumExemplaresDisponíveis(empréstimo_do_livro->livro->getNumExemplaresDisponíveis()+1)
+    try {
+        for (auto emprestimo : this->get_emprestimos()) {
+            if (caseInsensitiveComp(emprestimo->getLivro()->getTitulo(), titulo_livro_devolvido)) {
+                livro_devolvido = emprestimo;
+            }
+        }
+        if (livro_devolvido == nullptr) {
+            throw std::invalid_argument("O livro escolhido nao esta na sua lista de emprestimos, confira a ortografia (nao use acentos)");
+        }
+    } catch (std::invalid_argument &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    
+    if (livro_devolvido->calculaValorMulta() > 0.0) {
+        std::cout << "O livro esta atrasado " << livro_devolvido->getDiasDeAtraso() << 
+        ".\nA multa pelo atraso e de " << livro_devolvido->calculaValorMulta() << "\n";
 
+        if (this->get_carteirinha()->getSaldo() >= livro_devolvido->calculaValorMulta()) {
+            char resposta;
+            while(1) {
+                std::cout << "Deseja descontar a multa do saldo da carteirinha? (RS" << this->get_carteirinha()->getSaldo() << ") S/N: ";
+                
+                try {
+                    std::cin >> resposta;
+
+                    if (resposta != 'S' && resposta != 'N' &&
+                        resposta != 's' && resposta != 'n') 
+                        throw std::invalid_argument("Responda apenas com S para 'sim' ou N para 'nao'\n");
+                    
+                    break;
+                } catch (std::invalid_argument &e) {
+                    std::cerr << e.what() << std::endl;
+                }
+            }
+
+            if (resposta == 'S' || resposta == 's') {
+                this->get_carteirinha()->debitar(livro_devolvido->calculaValorMulta());
+            }
+            else {
+                std::cout << "Aguarde seu boleto esta sendo gerado\n";
+                /**
+                ...
+                ...
+                ...
+                **/
+            }
+        }
+    }
+
+    livro_devolvido->setDevolvido(true);
+    livro_devolvido->getLivro()->setNumExemplaresDisponiveis(livro_devolvido->getLivro()->getNumExemplaresDisponiveis()+1);
 }
+
 
 void Estudante::recarregarCarteirinha(){
     this->consultarSaldo(); // Opcional
